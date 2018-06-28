fdba8b1b45db5539d6bc6af733d811bd189abeb5
Michail Savvakis <mike@savvakis.net>
2018-06-28 19:48:27 +0300

    init

---
+++ routes/api.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('api', { title: 'Api' });
+});
+
+/* GET home page. */
+router.post('/sensit', function(req, res, next) {
+    console.log(req.body.glossary);
+    res.sendStatus(200);
+});
+
+module.exports = router;
